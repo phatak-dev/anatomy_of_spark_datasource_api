36e93331bcbaaf908e5707f96117396152981442
madhukar <phatak.dev@gmail.com>
2015-06-20 21:16:49 +0530

    sales sum example added

---
+++ src/main/scala/com/madhukaraphatak/spark/datasource/SalesSumExample.scala
@@ -0,0 +1,18 @@
+package com.madhukaraphatak.spark.datasource
+
+import org.apache.spark.SparkContext
+
+/**
+ * Created by madhu on 20/6/15.
+ */
+object SalesSumExample {
+  def main(args: Array[String]) {
+
+    val sc = new SparkContext(args(0), "Csv loading example")
+    val sqlContext = new org.apache.spark.sql.SQLContext(sc)
+    val df = sqlContext.load("com.madhukaraphatak.spark.datasource.csv", Map("path" -> args(1), "header" -> "true"))
+
+    println(df.agg(("amountPaid","sum")).collectAsList())
+
+  }
+}
